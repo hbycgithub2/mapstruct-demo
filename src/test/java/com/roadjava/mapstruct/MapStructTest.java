e225c4eab7ad18816fbf54302a2b190c1067801e
hbycgithub2 <1311768852@qq.com>
2025-04-28 23:28:49 +0800

    Reapply "fix: 修复了测试用例中价格格式化问题"
    This reverts commit 918e79e252dd4ed40aac0a35a3b2fa3fbc40d66a.

--- src/test/java/com/roadjava/mapstruct/MapStructTest.java
+++ src/test/java/com/roadjava/mapstruct/MapStructTest.java
@@ -161,6 +161,18 @@ public class MapStructTest {
         DriverVO driverVO = new DriverVO();
         DriverDTO driverDTO = carDTO.getDriverDTO();
         driverVO.setDriverId(driverDTO.getId());
+        System.out.println("mast-----1");
+        System.out.println("mast-----2");
+        System.out.println("mast-----3");
+        System.out.println("mast-----4");
+        System.out.println("mast-----5");
+        System.out.println("myMast------6");
+        System.out.println("myMast------6");
+        System.out.println("myMast------1");
+        System.out.println("myMast------2");
+        System.out.println("myMast------3");
+        System.out.println("myMast------4");
+        System.out.println("myMast------5");
         driverVO.setFullName(driverDTO.getName());
         carVO.setDriverVO(driverVO);
 
